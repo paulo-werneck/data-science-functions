01a28b02898843305751b96cd508e4e456198dd7
Paulo Werneck <paulo.werneck@powerofdata.com.br>
2020-06-04 21:28:06 -0300

    insert mkdir in travis.yml

--- tests/test_TuringClassInformationValueWoEMetrics.py
+++ tests/test_TuringClassInformationValueWoEMetrics.py
@@ -9,6 +9,7 @@ import os
 class TestWoe(TestCase):
 
     def setUp(self):
+        print(os.environ['PWD'])
         pwd = os.environ['PWD'].split('/')[:-1]
         pwd.append('utils')
         path = '/'.join(pwd)
