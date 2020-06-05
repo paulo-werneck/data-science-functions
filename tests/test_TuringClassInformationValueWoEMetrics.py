d6803deea9116902904165b2862bd44b6964d41c
Paulo Werneck <paulo.werneck@powerofdata.com.br>
2020-06-04 21:58:46 -0300

    b

--- tests/test_TuringClassInformationValueWoEMetrics.py
+++ tests/test_TuringClassInformationValueWoEMetrics.py
@@ -9,6 +9,7 @@ import os
 class TestWoe(TestCase):
 
     def setUp(self):
+        print(os.system('ls -la'))
         df = pd.read_csv('../utils/df_input_test.csv')
         self.woe, self.iv = TuringClassInformationValueWoEMetrics(df, 'target', ['blood_group', 'sex'])
 
